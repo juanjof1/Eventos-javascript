e898546ebec69d39e66b7fe16d2b21090b207cce
juanjof1 <salazargiljuanjose@gmail.com>
2021-06-11 16:03:37 -0600

    Eventos

---
+++ taller 6 eventos/JavaScript-Bubbling-and-Capturing-Workshop/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('button');
+const videoBox = document.querySelector('div');
+const video = document.querySelector('video');
+
+btn.onclick = function() {
+  videoBox.setAttribute('class','showing');
+}
+
+videoBox.onclick = function() {
+  videoBox.setAttribute('class','hidden');
+};
+
+video.onclick = function(event) {
+  event.stopPropagation();
+  video.play();
+};
+
+
+
+  
